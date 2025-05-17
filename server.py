ddb4a70c9283f85b7be0ff086e2c0ff9a577a0c8
Yurapchik <yuramn.9000@gmail.com>
2025-05-17 12:39:49 +0300

    Update server.py

--- server.py
+++ server.py
@@ -8,9 +8,16 @@ engine = create_engine("sqlite:///gamers.db")
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 s = Session()
+import socket
+
+
+def get_local_ip():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    return local_ip
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Настраиваем сокет
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Отключаем пакетирование
-main_socket.bind(("192.168.1.12", 10000))  # IP и порт привязываем к порту
+main_socket.bind((f"{get_local_ip()}", 10000))  # IP и порт привязываем к порту
 main_socket.setblocking(False)  # Непрерывность, не ждём ответа
 main_socket.listen(5)  # Прослушка входящих соединений, 5 одновременных подключений
 print("Сокет создался")
